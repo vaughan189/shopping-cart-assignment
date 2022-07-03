a51d188f13baf25427c167f664c30087454b342a
Vaughan Po <vaughan188@gmail.com>
2022-07-03 15:57:03 +0530

    No data found case handled

--- ui-app/src/pages/Products/components/ProductList/index.js
+++ ui-app/src/pages/Products/components/ProductList/index.js
@@ -1,4 +1,4 @@
-import { Grid } from "@mui/material";
+import { Alert, Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import React from "react";
 import { ProductItem } from "../ProductItem";
@@ -10,10 +10,35 @@ export const ProductList = (props) => {
       component={"div"}
       sx={{
         paddingLeft: { xl: "30px", md: "40px", sm: "0px", xs: "0px" },
-        paddingBottom: "20px",
+        paddingBottom: "4%",
       }}
     >
       <Grid container direction="row" spacing={2}>
+        {productsData.length === 0 && (
+          <Box
+            sx={{
+              marginLeft: "auto",
+              marginTop: "2%",
+              width:"100%"
+            }}
+          >
+            <Alert
+              severity="info"
+              color="primary"
+              sx={{
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "center",
+                fontFamily: "Dosis",
+                fontSize: 18,
+                fontWeight: 400,
+                width: "100%",
+              }}
+            >
+              No data found
+            </Alert>
+          </Box>
+        )}
         {productsData.map((item) => {
           return (
             <ProductItem
